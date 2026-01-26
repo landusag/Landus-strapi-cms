0235df9aee26d1c66cdeabed100056797422e732
Pavan Kumar <pavan.kumar@landus.ag>
2026-01-26 07:37:45 -0600

    redirect

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -15,7 +15,8 @@ export default [
       rules: [
         //{ from: "/old-slug", to: "/new-slug" },
         { from: "/events", to: "/company/events" },
-        { from: "/skyscout", to: "innovation/technologies/skyscout" },
+        //{ from: "/skyscout", to: "innovation/technologies/skyscout" },
+        { from: "/acreedge", to: "/businesses/agronomy/acreedge" },
         // Add as many as you need
       ],
     },
